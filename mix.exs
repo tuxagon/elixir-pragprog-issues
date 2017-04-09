f41486469a358aa40f72db6a4130af0074810cd1
Kenneth Bogner <bognerkenny@gmail.com>
2017-04-08 23:33:11 -0400

    Completed chapter 13

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config,
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/tuxagon/elixir-pragprog-issues",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -32,7 +34,9 @@ defmodule Issues.Mixfile do
     [
       {:httpoison, "~> 0.11"},
       {:poison, "~> 3.1"},
-      {:credo, "~> 0.7"}
+      {:credo, "~> 0.7"},
+      {:ex_doc, "~> 0.15"},
+      {:earmark, "~> 1.2", override: true}
     ]
   end
 
